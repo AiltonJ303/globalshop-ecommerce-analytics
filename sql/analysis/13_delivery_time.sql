2bc9c2d660416b0e4e76c4970f21c3870b24771d
AiltonJ303 <ailtonjose303@outlook.com>
2026-02-09 16:25:16 -0300

    Add delivery time analysis

---
+++ sql/analysis/13_delivery_time.sql
@@ -0,0 +1,6 @@
+USE globalshop;
+
+SELECT
+    ROUND(AVG(DATEDIFF(order_delivered_customer_date, order_purchase_timestamp)),2) AS avg_delivery_days
+FROM orders
+WHERE order_delivered_customer_date IS NOT NULL;
