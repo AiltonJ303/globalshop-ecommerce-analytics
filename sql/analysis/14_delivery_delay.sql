777597fe2b90ff4d695267bbc822249acf2d834c
AiltonJ303 <ailtonjose303@outlook.com>
2026-02-09 16:34:44 -0300

    Add delivery delay analysis

---
+++ sql/analysis/14_delivery_delay.sql
@@ -0,0 +1,12 @@
+	USE globalshop;
+
+SELECT
+    CASE
+        WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 'late'
+        ELSE 'on_time'
+    END AS delivery_status,
+    COUNT(*) AS total_orders,
+    ROUND(100 * COUNT(*) / SUM(COUNT(*)) OVER (), 2) AS percentage
+FROM orders
+WHERE order_delivered_customer_date IS NOT NULL
+GROUP BY delivery_status;
