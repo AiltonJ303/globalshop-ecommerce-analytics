d8b7439770f75a08d29603780a9545b963cf031b
AiltonJ303 <ailtonjose303@outlook.com>
2026-02-10 14:43:41 -0300

    Add new vs returning customers analysiss
    Added analysis section to understand customer acquisition and retention behavior.

--- sql/analysis/05_new_vs_returning_customers.sql
+++ sql/analysis/05_new_vs_returning_customers.sql
@@ -1,5 +1,23 @@
--- Analysis: New vs returning customers
--- Objective: understand retention behavior
+/*
+Analysis: Customer Acquisition
+
+Business Question:
+Is company growth driven by new or returning customers?
+
+Objective:
+Measure the number of new customers per month to understand acquisition behavior.
+
+Tables Used:
+- orders
+- customers
+
+Metric Created:
+- Monthly New Customers
+
+Author: Ailton 
+Project: GlobalShop E-commerce Analytics
+*/
+
 
 SELECT
     DATE_FORMAT(first_purchase, '%Y-%m') AS cohort_month,
